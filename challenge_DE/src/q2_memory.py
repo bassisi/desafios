bf857b8b62f79d1719addff264070cc6b817def0
Renato Bassi <renato.bassi@sansano.usm.cl>
2024-06-28 20:33:46 -0400

    Implementación de funcion q2_memory. Se utilizo la técnica de chunks para ahorrar memoria de uso. Utiliza Counter de la librería collections.

--- challenge_DE/src/q2_memory.py
+++ challenge_DE/src/q2_memory.py
@@ -1,4 +1,28 @@
 from typing import List, Tuple
+import pandas as pd
+import emoji
+from collections import Counter
+
+def extract_emojis(text):
+    # Extrae todos los emojis de un texto dado
+    return [char for char in text if char in emoji.EMOJI_DATA]
 
 def q2_memory(file_path: str) -> List[Tuple[str, int]]:
-    pass
+    # Leer el archivo en trozos
+    chunk_size = 10000
+    chunks = pd.read_json(file_path, lines=True, chunksize=chunk_size)
+    
+    # Contador para almacenar la frecuencia de los emojis
+    emoji_counter = Counter()
+    
+    for chunk in chunks:
+        # Asegurarse de que la columna "content" es de tipo string
+        chunk["content"] = chunk["content"].astype(str)
+        for content in chunk["content"]:
+            # Extraer emojis del contenido del tweet
+            emojis = extract_emojis(content)
+            # Actualizar el contador con los emojis extraídos
+            emoji_counter.update(emojis)
+    
+    # Devolver los 10 emojis más comunes con sus conteos
+    return emoji_counter.most_common(10)
